024029d3840a1857f89fbbce0cfd9c7724bd6224
Víctor Santos <victormss@gmail.com>
2019-10-11 10:22:41 +0200

    cambios

--- Mod1_Lab1/Program.cs
+++ Mod1_Lab1/Program.cs
@@ -13,10 +13,12 @@ namespace Mod1_Lab1
             int outer;
             int inner;
 
-            for (outer = 2; outer < 100; outer++)
+            for (outer = 2; outer < 10000000; outer++)
             {
                 for (inner = 2; inner <= (outer / inner); inner++)
                     if ((outer % inner) == 0) break;
+                if (inner > (outer / inner))
+                    Console.WriteLine("{0} is prime", outer);
             }
         }
     }
