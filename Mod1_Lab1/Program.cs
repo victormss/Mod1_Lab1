b47ab23be96195fb6d666df7c58ac5dabb3ffc93
Víctor Santos <victormss@gmail.com>
2019-10-11 10:11:48 +0200

    Agregar archivos de proyecto.

---
+++ Mod1_Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int outer;
+            int inner;
+
+            for (outer = 2; outer < 100; outer++)
+            {
+                for (inner = 2; inner <= (outer / inner); inner++)
+                    if ((outer % inner) == 0) break;
+            }
+        }
+    }
+}
